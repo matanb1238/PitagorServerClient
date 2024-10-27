fc487da0c131fed01b971be83a6342406f518353
matanb1238 <matanb1238@gmail.com>
2024-10-28 00:24:14 +0200

    Starting Version of section3 - now its the same code as section2

---
+++ Section3/Client/client.h
@@ -0,0 +1,12 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#define BUFFER_SIZE 1024
+
+int create_connection(const char *server_address, const char *server_port);
+
+void send_random_integer(int sockfd);
+
+void receive_response(int sockfd);
+
+#endif /* CLIENT_H */
