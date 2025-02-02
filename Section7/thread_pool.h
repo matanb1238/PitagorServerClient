8810ad45d0ce45d9df39e4d4e692f4999ea6559e
yoyo150696 <70186612+yoyo150696@users.noreply.github.com>
2025-02-02 22:44:04 +0200

    Delete Section7/thread_pool.h

--- Section7/thread_pool.h
+++
@@ -1,9 +0,0 @@
-// thread_pool.h
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
-
-void thread_pool_init(int num_threads);
-void thread_pool_add_task(void (*function)(void *), void *arg);
-void thread_pool_destroy();
-
-#endif  // THREAD_POOL_H
