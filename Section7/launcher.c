aa34780e50541fab9e795ea3882d590135b24d32
yoyo150696 <70186612+yoyo150696@users.noreply.github.com>
2025-02-02 22:43:09 +0200

    Update launcher.c

--- Section7/launcher.c
+++ Section7/launcher.c
@@ -1,43 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <pthread.h>
 
-#define NUM_CLIENTS 5 // Adjust the number of clients to spawn
-#define CLIENT_PROGRAM "./client" // Path to the compiled client program
+#define NUM_CLIENTS 5
+
+void *launch_client(void *arg) {
+    system("./client");
+    return NULL;
+}
 
 int main() {
-    pid_t pids[NUM_CLIENTS];
-
-    printf("Launcher: Starting %d clients...\n", NUM_CLIENTS);
+    pthread_t threads[NUM_CLIENTS];
 
     for (int i = 0; i < NUM_CLIENTS; i++) {
-        pids[i] = fork();
-
-        if (pids[i] == 0) {
-            // Child process: Run the client program
-            execl(CLIENT_PROGRAM, CLIENT_PROGRAM, NULL);
-
-            // If execl fails, print an error and exit
-            perror("execl failed");
-            exit(EXIT_FAILURE);
-        } else if (pids[i] < 0) {
-            // Fork failed
-            perror("fork failed");
-            exit(EXIT_FAILURE);
-        } else {
-            // Parent process: Continue spawning other clients
-            printf("Launcher: Started client %d (PID: %d)\n", i + 1, pids[i]);
-        }
+        pthread_create(&threads[i], NULL, launch_client, NULL);
     }
 
-    // Parent process: Wait for all child processes to finish
     for (int i = 0; i < NUM_CLIENTS; i++) {
-        waitpid(pids[i], NULL, 0);
-        printf("Launcher: Client %d (PID: %d) has finished.\n", i + 1, pids[i]);
+        pthread_join(threads[i], NULL);
     }
-
-    printf("Launcher: All clients have finished.\n");
 
     return 0;
 }
